b24f5729312a60ae6acc651b7b88c05a7b573d01
Sheilf <sheilf.code@gmail.com>
2022-05-21 16:40:12 -0700

    build: update gitignore for config data

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,23 +1,14 @@
 // Import the functions you need from the SDKs you need
 
 import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from "../config/firebaseConfig";
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: "tapdm3.firebaseapp.com",
-  projectId: "tapdm3",
-  storageBucket: "tapdm3.appspot.com",
-  messagingSenderId: "818353276964",
-  appId: "1:818353276964:web:524e672d4286e13e1bb0b1",
-  measurementId: "G-RM1ZCHHDEZ"
-};
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
